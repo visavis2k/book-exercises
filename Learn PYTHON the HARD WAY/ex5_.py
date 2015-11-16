ed0dd1dfd83d7730ec30a94ca4337cdecd5d8b06
Kyutae Park <visavis2k@gmail.com>
2015-11-16 13:07:47 +0900

    5~6

---
+++ Learn PYTHON the HARD WAY/ex5_.py
@@ -0,0 +1,22 @@
+# -*- coding: cp949 -*-
+
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 188 / 2.52 # 인치
+weight = 82 / 0.4536 # 파운드
+eyes = '파랑'
+teeth = '하양'
+hair = '갈색'
+
+print "%s에 대해 이야기해 보죠." % name
+print "키는 %d 인치고요." % height
+print "몸무게는 %d 파운드이에요." % weight
+print "사실 아주 많이 나가는 건 아니죠."
+print "눈은 %s이고 머리는 %s이에요." % (eyes, hair)
+print "이는 보통 %s이고 커피를 먹으면 달라져요." % teeth
+
+# 이 줄은 까다롭지만 정확히 따라 하세요
+print "%d, %d, %d를 모두 더하면 %d 랍니다." % (age, height, weight, age + height + weight)
+
+# 파이썬의 새로운 포맷팅 방식에 대해서 소개하는 웹페이지
+# https://pyformat.info/
