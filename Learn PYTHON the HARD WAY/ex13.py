bfd267539212358ba475fbe3255171c6f358f782
Kyutae Park <visavis2k@gmail.com>
2015-11-20 08:03:21 +0900

    13~14

---
+++ Learn PYTHON the HARD WAY/ex13.py
@@ -0,0 +1,12 @@
+# -*- coding: cp949 -*-
+# 매개변수, 풀기(Unpack), 변수
+from sys import argv
+
+script, first, second, third = argv
+
+print "스크립트 이름:", script
+print "첫 번째 변수:", first
+print "두 번째 변수:", second
+print "세 번째 변수:", third
+
+
