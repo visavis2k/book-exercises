ce5c69c44f6043c7e8b94d131c744248d9b6156a
Kyutae Park <visavis2k@gmail.com>
2015-11-21 09:13:56 +0900

    15

---
+++ Learn PYTHON the HARD WAY/ex15.py
@@ -0,0 +1,22 @@
+# -*- coding: cp949 -*-
+#
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print "파일 %r의 내용:" % filename
+print txt.read()
+
+txt.close()
+
+print "파일 이름을 다시 입력해 주세요."
+file_again = raw_input("> ")
+
+txt_again = open(file_again)
+
+print txt_again.read()
+
+txt_again.close()
+
