781b3d74418434b297af1a56833911b8f80d0c65
Kyutae Park <visavis2k@gmail.com>
2015-11-30 21:58:27 +0900

    34

---
+++ Learn PYTHON the HARD WAY/ex34.py
@@ -0,0 +1,13 @@
+# -*- coding: cp949 -*-
+# 리스트 원소 접근
+animals = ['곰', '비단뱀', '공작', '캥거루', '고래', '오리너구리']
+print animals[1] # 1에 있는 동물. 비단뱀
+print animals[2] # 세 번째 동물. 공작
+print animals[0] # 첫 번째 동물. 곰
+print animals[3] # 3에 있는 동물. 캥거루
+print animals[4] # 다섯 번째 동물. 고래
+print animals[2] # 2에 있는 동물. 공작
+print animals[5] # 여섯 번째 동물. 오리너구리
+print animals[4] # 4에 있는 동물. 고래
+
+
